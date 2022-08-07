26cd76f3b1eb9f25aa2ebea9bebb0a9385131a98
Keith Wickramasekara <keith.wi@live.com>
2022-08-06 19:06:20 -0500

    Convert templates to nunjucks

--- .eleventy.js
+++ .eleventy.js
@@ -3,16 +3,12 @@ const dayjs = require("dayjs");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/styles/");
 
-  eleventyConfig.addHandlebarsHelper("formatDate", function (dateString) {
+  eleventyConfig.addNunjucksFilter("formatDate", function (dateString) {
     return dayjs(dateString).format("	MMM D, YYYY");
   });
 
-  eleventyConfig.addHandlebarsHelper("filterObject", function (object, key) {
-    return object[key];
-  });
-
   // https://w3collective.com/calculate-reading-time-javascript/
-  eleventyConfig.addHandlebarsHelper("readingTime", function (content) {
+  eleventyConfig.addNunjucksFilter("readingTime", function (content) {
     const wpm = 225;
     const words = content.trim().split(/\s+/).length;
 
