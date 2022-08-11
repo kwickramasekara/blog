9c7f3d437bf7be288501525137f2f74b38e766db
Keith Wickramasekara <keith.wi@live.com>
2022-08-11 18:48:19 -0500

    Set site description

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 const dayjs = require("dayjs");
+const njkFilters = require("nunjucks/src/filters");
+
+const META_DESCRIPTION_LENGTH = 160; // https://moz.com/learn/seo/meta-description
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/styles/");
@@ -15,6 +18,35 @@ module.exports = function (eleventyConfig) {
     const words = content.trim().split(/\s+/).length;
 
     return Math.ceil(words / wpm);
+  });
+
+  eleventyConfig.addNunjucksFilter("metaDescription", function (content) {
+    if (content) {
+      let paragraphContent = content.split("<p>");
+
+      // get the first paragraph element
+      paragraphContent =
+        paragraphContent.length > 2 ? paragraphContent[1].split("</p>") : null;
+
+      // convert array to string
+      paragraphContent = paragraphContent
+        ? njkFilters.string(paragraphContent)
+        : null;
+
+      // convert any anchor elements to text
+      paragraphContent = paragraphContent
+        ? njkFilters.striptags(paragraphContent)
+        : null;
+
+      // limit description length to SEO standards
+      paragraphContent = paragraphContent
+        ? njkFilters.truncate(paragraphContent, META_DESCRIPTION_LENGTH)
+        : null;
+
+      return paragraphContent;
+    }
+
+    return null;
   });
 
   return {
--- src/_data/site.js
+++ src/_data/site.js
@@ -2,4 +2,6 @@ module.exports = {
   author: "Keith Wickramasekara",
   copyrightYear: new Date().getFullYear(),
   title: "Blog",
+  description:
+    "Hey! I'm Keith Wickramasekara, a software engineer by profession and a photographer by hobby. This is a small space on the internet where I drop my thoughts and ideas on those two topics and my other interests like personal finance.",
 };
